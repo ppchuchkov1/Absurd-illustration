3ed84de2a4d1d5a47d171859272937ae4c6bdd83
ppchuchkov1 <59335803+ppchuchkov1@users.noreply.github.com>
2020-10-13 15:36:53 +0300

    Add files via upload

---
+++ Absurd illustrations/js/script.js
@@ -0,0 +1,308 @@
+$('.carousel').carousel({
+    interval: 2500
+  });
+
+  document.addEventListener('DOMContentLoaded', (event) => {
+    anime({
+        targets: '#home-section',
+        translateX: [-250,0],
+        delay:1000,
+        opacity: [0,1],
+      });
+      anime({
+        targets: 'nav',
+        translateX: [-250,0],
+        delay:500,
+        opacity: [0,1],
+        easing: 'easeInOutSine',
+      });
+});
+
+var waypoints = $('#section-2').waypoint(function(direction) {
+  anime({
+    targets: '#section-2',
+    translateX: [100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
+
+var waypoints = $('#section-3').waypoint(function(direction) {
+  anime({
+    targets: '#section-3',
+    translateX: [-100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
+
+var waypoints = $('#section-4').waypoint(function(direction) {
+  anime({
+    targets: '#section-4',
+    translateX: [100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
+
+var waypoints = $('#section-5').waypoint(function(direction) {
+  anime({
+    targets: '#section-5',
+    translateY: [-100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
+
+var waypoints = $('#section-6').waypoint(function(direction) {
+  anime({
+    targets: '.card-1',
+    translateX: [100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
+
+var waypoints = $('#section-6').waypoint(function(direction) {
+  anime({
+    targets: '.card-2',
+    translateY: [100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
+
+
+var waypoints = $('#section-6').waypoint(function(direction) {
+  anime({
+    targets: '.card-3',
+    translateY: [100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
+
+
+var waypoints = $('#section-6').waypoint(function(direction) {
+  anime({
+    targets: '.card-4',
+    translateX: [-100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
+
+
+var waypoints = $('#section-7').waypoint(function(direction) {
+  anime({
+    targets: '#section-7',
+    translateY: [100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
+
+
+var waypoints = $('#section-8').waypoint(function(direction) {
+  anime({
+    targets: '#section-8',
+    translateY: [100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
+
+var waypoints = $('#section-9').waypoint(function(direction) {
+  anime({
+    targets: '#section-9',
+    translateY: [100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
+
+var waypoints = $('#section-10').waypoint(function(direction) {
+  anime({
+    targets: '#section-10',
+    translateY: [100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
+
+var waypoints = $('#section-11').waypoint(function(direction) {
+  anime({
+    targets: '#section-11',
+    translateY: [100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
+var waypoints = $('#section-11').waypoint(function(direction) {
+  anime({
+    targets: '#free-card',
+    translateX: [100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
+
+var waypoints = $('#section-11').waypoint(function(direction) {
+  anime({
+    targets: '#membership-card',
+    translateX: [-100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
+
+var waypoints = $('#for-more').waypoint(function(direction) {
+  anime({
+    targets: '#for-more',
+    translateY: [100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
+
+var waypoints = $('#section-12').waypoint(function(direction) {
+  anime({
+    targets: '#section-12',
+    translateY: [100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
+
+var waypoints = $('#section-13').waypoint(function(direction) {
+  anime({
+    targets: '#section-13',
+    translateY: [100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
+
+var waypoints = $('#section-14').waypoint(function(direction) {
+  anime({
+    targets: '#section-14',
+    translateY: [100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
+
+var waypoints = $('#section-15').waypoint(function(direction) {
+  anime({
+    targets: '#section-15',
+    translateY: [100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
+
+var waypoints = $('#section-16').waypoint(function(direction) {
+  anime({
+    targets: '#section-16',
+    translateY: [100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
+
+var waypoints = $('#section-17').waypoint(function(direction) {
+  anime({
+    targets: '#section-17',
+    translateY: [100 ,0],
+    delay:100,
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+  });
+  this.destroy()
+}, {
+  offset: '80%'
+})
